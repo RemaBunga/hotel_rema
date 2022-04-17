a0070d76815194e1357ae6f92176976487d94165
RemaBunga <remabunga@gmail.com>
2022-04-17 07:38:07 +0700

    perubahan1

---
+++ application/views/content/fasilitas.php
@@ -0,0 +1,59 @@
+<!-- Portfolio Grid-->
+<section class="page-section bg-light" id="portfolio">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Fasilitas</h2>
+                    <h3 class="section-subheading text-muted">Ini Fasilitas terbaik di hotel kami</h3>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-lg-4 col-sm-6 mb-4">
+                        <!-- Portfolio item 1-->
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-bs-toggle="modal" href="#portfolioModal7">                               
+                                <img class="img-fluid" src="<?= base_url('img/'); ?>/kamar/kolam.jpg" alt="..." />
+                            </a>
+                            <hr>
+                            <div class="portfolio-caption text-center">
+                                <form action="">
+                                    <button class="btn btn-primary btn-block">Fasilitas Kolam Renang</button>
+                                </form>
+                                <div class="portfolio-caption-heading"></div>
+                                <div class="portfolio-caption-subheading text-muted">Fasilitas dari semua jenis kamar</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 mb-4">
+                        <!-- Portfolio item 2-->
+                        <div class="portfolio-item ">
+                            <a class="portfolio-link" data-bs-toggle="modal" href="#portfolioModal8">
+                                <img class="img-fluid" src="<?= base_url('img/'); ?>/kamar/taman.jpg" alt="..." />
+                            </a>
+                            <hr>
+                            <div class="portfolio-caption text-center">
+                                <form action="">
+                                    <button class="btn btn-primary btn-block">Fasilitas Taman Hotel </button>
+                                </form>
+                                <div class="portfolio-caption-heading"></div>
+                                <div class="portfolio-caption-subheading text-muted">Di hotel kami menyediakan taman untuk anda bersantai dengan keluarga /Patner anda</div>
+                            </div>
+                        </div>
+                    </div>
+                  
+                    <div class="col-lg-4 col-sm-6 mb-4">
+                        <!-- Portfolio item 2-->
+                        <div class="portfolio-item ">
+                            <a class="portfolio-link" data-bs-toggle="modal" href="#portfolioModal9">
+                                <img class="img-fluid" src="<?= base_url('img/'); ?>/kamar/restoran.jpg" alt="..." />
+                            </a>
+                            <hr>
+                            <div class="portfolio-caption text-center">
+                                <form action="">
+                                    <button class="btn btn-primary btn-block">Fasilitas Restoran Hotel </button>
+                                </form>
+                                <div class="portfolio-caption-heading"></div>
+                                <div class="portfolio-caption-subheading text-muted">dihotel kami menyediakan fasilitas restoran terbaik</div>
+                            </div>
+                        </div>
+                    </div>
+        </section>
---
+++ application/views/content/promo.php
@@ -0,0 +1,38 @@
+<!-- Team-->
+<section class="page-section bg-light" id="team">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Promo</h2>
+                    <h3 class="section-subheading text-muted">Promo bulan April-juni 2022</h3>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="<?= base_url('assets/'); ?>/assets/img/team/4.jpg" alt="..." />
+                            <h4>Superior Room</h4>
+                            <p class="text-muted">Diskon 50% dari harga Rp.700.000 menjadi Rp350.000</p>
+                           
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="<?= base_url('assets/'); ?>/assets/img/team/5.jpg" alt="..." />
+                            <h4>Junior Room</h4>
+                            <p class="text-muted">Diskon 30%  dari harga Rp.700.000 menjadi Rp490.000</p>
+                            
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="<?= base_url('assets/'); ?>/assets/img/team/6.jpg" alt="..." />
+                            <h4>Family Room</h4>
+                            <p class="text-muted">Diskon 50%  dari harga Rp.700.000 menjadi Rp350.000</p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Promo kami hanya berlaku sampai akhir bulan yang telah di tentukan di Hotel kami.</p></div>
+                </div>
+            </div>
+        </section>
